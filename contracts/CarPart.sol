dc6d6f6a74c5703886fe223f1c43689945aecbaf
DennisChi <dennischi99@gmail.com>
2024-08-29 11:52:11 +0800

    Updated the token implementation.

--- contracts/CarPart.sol
+++ contracts/CarPart.sol
@@ -4,13 +4,16 @@ pragma solidity ^0.8.20;
 import {ICarPart, Part} from "./interfaces/ICarPart.sol";
 import {ICarFactory} from "./interfaces/ICarFactory.sol";
 
-import {IERC165} from "@openzeppelin/contracts/interfaces/IERC165.sol";
-import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
-import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
-import {IERC1155Receiver} from "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
+import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+import {IERC1155MetadataURI} from "@openzeppelin/contracts/token/ERC1155/extensions/IERC1155MetadataURI.sol";
+
+import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract CarPart is ICarPart, AccessControl {
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+
+contract CarPart is ICarPart, ERC1155, AccessControl {
     bytes32 public constant CAR_FACTORY_ROLE = keccak256("CAR_FACTORY_ROLE");
 
     bytes32 public constant PART_TYPES =
@@ -19,167 +22,49 @@ contract CarPart is ICarPart, AccessControl {
             keccak256("Engine") |
             keccak256("Tires");
 
-    mapping(address => mapping(address => bool)) private _operatorApprovals;
-    mapping(address => mapping(uint256 => uint256)) private _balances;
     mapping(uint256 => Part) private _partOf;
 
-    modifier validSender(address sender) {
-        if (sender == address(0)) {
-            revert ERC1155InvalidSender(sender);
-        }
-        _;
-    }
-
-    modifier validReceiver(address receiver) {
-        if (receiver == address(0)) {
-            revert ERC1155InvalidReceiver(receiver);
-        }
-        _;
-    }
-
-    modifier validApprovalForAll(address operator) {
-        if (
-            operator != msg.sender && !_operatorApprovals[msg.sender][operator]
-        ) {
-            revert ERC1155MissingApprovalForAll(operator, msg.sender);
-        }
-        _;
-    }
-
-    modifier validOperator(address operator) {
-        if (operator == address(0)) {
-            revert ERC1155InvalidOperator(operator);
-        }
-        _;
-    }
-
-    modifier validArrayLength(uint256 idsLength, uint256 valuesLength) {
-        if (idsLength != valuesLength) {
-            revert ERC1155InvalidArrayLength(idsLength, valuesLength);
-        }
-        _;
-    }
-
-    constructor(address carFactoryAddress_) {
+    constructor(address carFactoryAddress_) ERC1155("") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(CAR_FACTORY_ROLE, carFactoryAddress_);
 
         _setRoleAdmin(CAR_FACTORY_ROLE, DEFAULT_ADMIN_ROLE);
     }
 
-    function setApprovalForAll(
-        address operator,
-        bool approved
-    ) external validOperator(operator) {
-        _operatorApprovals[msg.sender][operator] = approved;
-        emit ApprovalForAll(msg.sender, operator, approved);
-    }
-
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 id,
-        uint256 amount,
-        bytes memory data
-    )
-        external
-        validSender(from)
-        validReceiver(to)
-        validApprovalForAll(msg.sender)
-    {
-        _safeTransferFrom(from, to, id, amount);
-
-        if (to.code.length != 0) {
-            bool success = IERC1155Receiver(to).onERC1155Received(
-                msg.sender,
-                from,
-                id,
-                amount,
-                data
-            ) == IERC1155Receiver.onERC1155Received.selector;
-            if (!success) {
-                revert ERC1155InvalidReceiver(to);
-            }
-        }
-
-        emit TransferSingle(msg.sender, from, to, id, amount);
-    }
-
-    function safeBatchTransferFrom(
-        address from,
-        address to,
-        uint256[] memory ids,
-        uint256[] memory amounts,
-        bytes memory data
-    )
-        external
-        validSender(from)
-        validReceiver(to)
-        validApprovalForAll(msg.sender)
-        validArrayLength(ids.length, amounts.length)
-    {
-        for (uint256 i = 0; i < ids.length; i++) {
-            _safeTransferFrom(from, to, ids[i], amounts[i]);
-        }
-
-        if (to.code.length != 0) {
-            bool success = IERC1155Receiver(to).onERC1155BatchReceived(
-                msg.sender,
-                from,
-                ids,
-                amounts,
-                data
-            ) == IERC1155Receiver.onERC1155BatchReceived.selector;
-            if (!success) {
-                revert ERC1155InvalidReceiver(to);
-            }
-        }
-
-        emit TransferBatch(msg.sender, from, to, ids, amounts);
-    }
-
     function mint(
         address to,
         uint256 id
-    ) external override onlyRole(CAR_FACTORY_ROLE) validReceiver(to) {
-        _mint(to, id);
-        emit TransferSingle(msg.sender, address(0), to, id, 1);
+    ) external override onlyRole(CAR_FACTORY_ROLE) {
+        _mint(to, id, 1, "");
     }
 
     function burn(
         address from,
         uint256 id
-    ) external override onlyRole(CAR_FACTORY_ROLE) validReceiver(from) {
-        _burn(from, id);
-        emit TransferSingle(msg.sender, from, address(0), id, 1);
+    ) external override onlyRole(CAR_FACTORY_ROLE) {
+        _burn(from, id, 1);
     }
 
     function batchMint(
         address to,
         uint256[] memory ids
-    ) external override onlyRole(CAR_FACTORY_ROLE) validReceiver(to) {
-        for (uint256 i = 0; i < ids.length; i++) {
-            _mint(to, ids[i]);
-        }
+    ) external override onlyRole(CAR_FACTORY_ROLE) {
         uint256[] memory amounts = new uint256[](ids.length);
         for (uint256 i = 0; i < ids.length; i++) {
             amounts[i] = 1;
         }
-        emit TransferBatch(msg.sender, address(0), to, ids, amounts);
+        _mintBatch(to, ids, amounts, "");
     }
 
     function batchBurn(
         address from,
         uint256[] memory ids
-    ) external override onlyRole(CAR_FACTORY_ROLE) validReceiver(from) {
-        for (uint256 i = 0; i < ids.length; i++) {
-            _burn(from, ids[i]);
-        }
+    ) external override onlyRole(CAR_FACTORY_ROLE) {
         uint256[] memory amounts = new uint256[](ids.length);
         for (uint256 i = 0; i < ids.length; i++) {
             amounts[i] = 1;
         }
-        emit TransferBatch(msg.sender, from, address(0), ids, amounts);
+        _burnBatch(from, ids, amounts);
     }
 
     function addParts(Part[] memory parts) external {
@@ -189,7 +74,7 @@ contract CarPart is ICarPart, AccessControl {
         ) {
             revert AccessControlUnauthorizedAccount(
                 msg.sender,
-                DEFAULT_ADMIN_ROLE | CAR_FACTORY_ROLE
+                DEFAULT_ADMIN_ROLE
             );
         }
         for (uint256 i = 0; i < parts.length; i++) {
@@ -218,34 +103,28 @@ contract CarPart is ICarPart, AccessControl {
         }
     }
 
-    function balanceOf(
-        address account,
-        uint256 id
-    ) external view returns (uint256) {
-        return _balances[account][id];
+    function partOf(uint256 tokenId) external view returns (Part memory part) {
+        return _partOf[tokenId];
     }
 
-    function balanceOfBatch(
-        address[] memory accounts,
-        uint256[] memory ids
-    ) external view returns (uint256[] memory) {
-        uint256[] memory balances = new uint256[](accounts.length);
-        unchecked {
-            for (uint256 i = 0; i < accounts.length; i++) {
-                balances[i] = _balances[accounts[i]][ids[i]];
-            }
+    function partsOf(
+        uint256[] memory partIds
+    ) external view returns (Part[] memory parts) {
+        parts = new Part[](partIds.length);
+        for (uint256 i = 0; i < partIds.length; i++) {
+            parts[i] = _partOf[partIds[i]];
         }
-        return balances;
+        return parts;
     }
 
-    function isApprovedForAll(
-        address account,
-        address operator
-    ) external view returns (bool) {
-        return _operatorApprovals[account][operator];
-    }
-
-    function uri(uint256 id) external view returns (string memory) {
+    function uri(
+        uint256 id
+    )
+        public
+        view
+        override(ERC1155, IERC1155MetadataURI)
+        returns (string memory)
+    {
         Part memory part = _partOf[id];
         string memory json = string(
             abi.encodePacked(
@@ -270,56 +149,17 @@ contract CarPart is ICarPart, AccessControl {
             );
     }
 
-    function partOf(uint256 tokenId) external view returns (Part memory part) {
-        return _partOf[tokenId];
-    }
-
-    function partsOf(
-        uint256[] memory partIds
-    ) external view returns (Part[] memory parts) {
-        parts = new Part[](partIds.length);
-        for (uint256 i = 0; i < partIds.length; i++) {
-            parts[i] = _partOf[partIds[i]];
-        }
-        return parts;
-    }
-
     function supportsInterface(
         bytes4 interfaceId
-    ) public view virtual override(AccessControl, IERC165) returns (bool) {
+    )
+        public
+        view
+        virtual
+        override(AccessControl, ERC1155, IERC165)
+        returns (bool)
+    {
         return
             interfaceId == type(ICarPart).interfaceId ||
             super.supportsInterface(interfaceId);
-    }
-
-    function _safeTransferFrom(
-        address from,
-        address to,
-        uint256 id,
-        uint256 amount
-    ) internal {
-        uint256 fromBalance = _balances[from][id];
-        if (fromBalance < amount) {
-            revert ERC1155InsufficientBalance(from, fromBalance, amount, id);
-        }
-
-        unchecked {
-            _balances[from][id] = fromBalance - amount;
-        }
-        _balances[to][id] += amount;
-    }
-
-    function _mint(address to, uint256 id) internal validReceiver(to) {
-        _balances[to][id] += 1;
-    }
-
-    function _burn(address from, uint256 id) internal {
-        uint256 fromBalance = _balances[from][id];
-        if (fromBalance == 0) {
-            revert ERC1155InsufficientBalance(from, fromBalance, 0, id);
-        }
-        unchecked {
-            _balances[from][id] = fromBalance - 1;
-        }
     }
 }
--- contracts/RaceCar.sol
+++ contracts/RaceCar.sol
@@ -5,60 +5,31 @@ import {ICarPart, Part} from "./interfaces/ICarPart.sol";
 import {IRaceCar} from "./interfaces/IRaceCar.sol";
 import {ICarFactory} from "./interfaces/ICarFactory.sol";
 
+import {IERC721Metadata} from "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
 import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
-import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
-import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract RaceCar is IRaceCar, AccessControl {
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+
+contract RaceCar is IRaceCar, ERC721, AccessControl {
     bytes32 public constant CAR_FACTORY_ROLE = keccak256("CAR_FACTORY_ROLE");
 
-    string private _name;
-    string private _symbol;
-
-    mapping(address => uint256) private _balanceOf;
-    mapping(uint256 => address) private _owners;
-    mapping(uint256 => address) private _approvals;
-    mapping(address => mapping(address => bool)) private _operatorApprovals;
     // carId => partIds
     mapping(uint256 => uint256[]) private _partsOf;
 
-    uint256 private _maxTokenId;
-
     address private _carPart;
 
-    modifier validAddress(address addr) {
-        if (addr == address(0)) {
-            revert ERC721InvalidOwner(addr);
-        }
-        _;
-    }
-
-    modifier validToken(uint256 tokenId) {
-        if (_owners[tokenId] == address(0)) {
-            revert ERC721NonexistentToken(tokenId);
-        }
-        _;
-    }
-
-    modifier tokenOwnerOnly(uint256 tokenId) {
-        address owner = _owners[tokenId];
-        if (msg.sender != owner) {
-            revert ERC721InvalidApprover(msg.sender);
-        }
-        _;
-    }
+    uint256 private _maxTokenId;
 
     constructor(
         string memory name_,
         string memory symbol_,
         address carFactoryAddress_,
         address carPartAddress_
-    ) {
-        _name = name_;
-        _symbol = symbol_;
+    ) ERC721(name_, symbol_) {
         _carPart = carPartAddress_;
 
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
@@ -67,113 +38,26 @@ contract RaceCar is IRaceCar, AccessControl {
         _setRoleAdmin(CAR_FACTORY_ROLE, DEFAULT_ADMIN_ROLE);
     }
 
-    function transferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-    ) external validAddress(from) validAddress(to) validToken(tokenId) {
-        _transferFrom(from, to, tokenId);
-    }
-
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-    ) external validAddress(from) validAddress(to) validToken(tokenId) {
-        _safeTransferFrom(from, to, tokenId, "");
-    }
-
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 tokenId,
-        bytes calldata data
-    ) external validAddress(from) validAddress(to) validToken(tokenId) {
-        _safeTransferFrom(from, to, tokenId, data);
-    }
-
-    function approve(
-        address to,
-        uint256 tokenId
-    ) external validAddress(to) validToken(tokenId) tokenOwnerOnly(tokenId) {
-        address owner = _owners[tokenId];
-        _approvals[tokenId] = to;
-        emit Approval(owner, to, tokenId);
-    }
-
-    function setApprovalForAll(
-        address operator,
-        bool approved
-    ) external validAddress(operator) {
-        _operatorApprovals[msg.sender][operator] = approved;
-        emit ApprovalForAll(msg.sender, operator, approved);
-    }
-
     function mint(
         address to,
         uint256[] memory partIds
-    )
-        external
-        override
-        onlyRole(CAR_FACTORY_ROLE)
-        validAddress(to)
-        returns (uint256 tokenId)
-    {
+    ) external override onlyRole(CAR_FACTORY_ROLE) returns (uint256 tokenId) {
         tokenId = ++_maxTokenId;
-        _balanceOf[to]++;
-        _owners[tokenId] = to;
+        _safeMint(to, tokenId);
         _partsOf[tokenId] = partIds;
-
-        emit Transfer(address(0), to, tokenId);
     }
 
     function burn(
         uint256 tokenId
-    ) external override onlyRole(CAR_FACTORY_ROLE) validToken(tokenId) {
-        address owner = _owners[tokenId];
-        _balanceOf[owner]--;
-        delete _owners[tokenId];
-        delete _approvals[tokenId];
-
-        emit Transfer(owner, address(0), tokenId);
-    }
-
-    function balanceOf(
-        address owner
-    ) external view validAddress(owner) returns (uint256) {
-        return _balanceOf[owner];
-    }
-
-    function getApproved(
-        uint256 tokenId
-    ) external view validToken(tokenId) returns (address) {
-        return _approvals[tokenId];
-    }
-
-    function isApprovedForAll(
-        address owner,
-        address operator
-    ) external view validAddress(owner) validAddress(operator) returns (bool) {
-        return _operatorApprovals[owner][operator];
-    }
-
-    function ownerOf(
-        uint256 tokenId
-    ) external view validToken(tokenId) returns (address) {
-        return _owners[tokenId];
-    }
-
-    function name() external view override returns (string memory) {
-        return _name;
-    }
-
-    function symbol() external view override returns (string memory) {
-        return _symbol;
+    ) external override onlyRole(CAR_FACTORY_ROLE) {
+        _burn(tokenId);
+        delete _partsOf[tokenId];
     }
 
     function tokenURI(
         uint256 tokenId
-    ) external view override validToken(tokenId) returns (string memory) {
+    ) public view override(ERC721, IERC721Metadata) returns (string memory) {
+        _requireOwned(tokenId);
         uint256[] memory partIds = _partsOf[tokenId];
         Part[] memory parts = ICarPart(_carPart).partsOf(partIds);
         string memory svgImage = '<svg xmlns="http://www.w3.org/2000/svg">';
@@ -208,11 +92,11 @@ contract RaceCar is IRaceCar, AccessControl {
         string memory json = string(
             abi.encodePacked(
                 '{"name": "',
-                _name,
+                name(),
                 " #",
                 Strings.toString(tokenId),
                 '", "description": "',
-                _symbol,
+                symbol(),
                 ' NFT",',
                 '"image": "data:image/svg+xml;base64,',
                 Base64.encode(bytes(svgImage)),
@@ -234,57 +118,16 @@ contract RaceCar is IRaceCar, AccessControl {
 
     function partsOf(
         uint256 tokenId
-    ) external view validToken(tokenId) returns (uint256[] memory partIds) {
+    ) external view returns (uint256[] memory partIds) {
+        _requireOwned(tokenId);
         return _partsOf[tokenId];
     }
 
     function supportsInterface(
         bytes4 interfaceId
-    ) public view override(AccessControl, IERC165) returns (bool) {
+    ) public view override(ERC721, AccessControl, IERC165) returns (bool) {
         return
             interfaceId == type(IRaceCar).interfaceId ||
             super.supportsInterface(interfaceId);
-    }
-
-    function _safeTransferFrom(
-        address from,
-        address to,
-        uint256 tokenId,
-        bytes memory data
-    ) internal {
-        _transferFrom(from, to, tokenId);
-
-        if (to.code.length != 0) {
-            bool success = IERC721Receiver(to).onERC721Received(
-                msg.sender,
-                from,
-                tokenId,
-                data
-            ) == IERC721Receiver.onERC721Received.selector;
-            if (!success) {
-                revert ERC721InvalidReceiver(to);
-            }
-        }
-    }
-
-    function _transferFrom(address from, address to, uint256 tokenId) internal {
-        address owner = _owners[tokenId];
-        if (owner != from) {
-            revert ERC721IncorrectOwner(from, tokenId, owner);
-        }
-
-        bool isApprovedOrOwner = (msg.sender == owner) ||
-            (msg.sender == _approvals[tokenId]) ||
-            _operatorApprovals[owner][msg.sender];
-        if (!isApprovedOrOwner) {
-            revert ERC721InsufficientApproval(msg.sender, tokenId);
-        }
-
-        _balanceOf[from]--;
-        _balanceOf[to]++;
-        _owners[tokenId] = to;
-        delete _approvals[tokenId];
-
-        emit Transfer(from, to, tokenId);
     }
 }
